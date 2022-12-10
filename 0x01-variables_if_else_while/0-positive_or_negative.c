166cfe214805d00b616b0a137f75a6f6920f0535
fem-itech <femielijaholubodun@gmail.com>
2022-12-10 08:10:40 -0800

    workonif

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,15 +3,12 @@
 #include <time.h>
 /**
  * main - Entry point
- * 
  * Description: 'Assign random number to n'
- * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n >  0)

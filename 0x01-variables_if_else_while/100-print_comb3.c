01d461b08c6b0651736e8a9f7d0815a6d8f6c2cd
fem-itech <femielijaholubodun@gmail.com>
2022-12-11 14:22:54 -0800

    bler
    Signed-off-by: fem-itech <femielijaholubodun@gmail.com>

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,15 @@ int main(void)
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
+		putchar(num);
 		if (num == '9')
 		{
 			break;
 		}
 		else
 		{
-			putchar(',');
-			putchar(' ');
+			putchar('num');
+			putchar('num');
 		}
 	}
 	putchar('\n');

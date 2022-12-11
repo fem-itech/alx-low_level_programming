0c7a8ca35a4c78881ee1169dfbcf6d58730ced68
fem-itech <femielijaholubodun@gmail.com>
2022-12-10 20:37:11 -0800

    capital and small letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry of the code
+ * Description: 'the program to print out a-z in lower and uppercase'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	char S;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (S = 'A'; S <= 'Z'; S++)
+	{
+		putchar(S);
+	}
+	putchar('\n');
+	return(0);
+}

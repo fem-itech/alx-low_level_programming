008df76e61196794c29e1d8aac264c5ce42ad545
fem-itech <femielijaholubodun@gmail.com>
2022-12-10 21:05:12 -0800

    exept

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry of the code
+ * Description: 'print out alphabet aside a and q'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e' || i == 'q')
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);

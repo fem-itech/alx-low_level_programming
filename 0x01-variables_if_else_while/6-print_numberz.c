1871e4184e9dc18393b7af4f52f3ea86f82fe801
fem-itech <femielijaholubodun@gmail.com>
2022-12-11 07:08:23 -0800

    printnumber6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry of the code
+ * Description: 'the program to printz'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i='0'; i<='9'; i++)
+{
+	putchar(i);
+}
+	putchar('\n');
+	return (0);
+}

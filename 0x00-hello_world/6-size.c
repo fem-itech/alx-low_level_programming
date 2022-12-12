22fec61860798caadf3f99d0272e437f85c6893d
fem-itech <femielijaholubodun@gmail.com>
2022-12-11 21:00:18 -0800

    oyes 5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,11 @@
  */
 int main(void)
 {
+	char charType;
+        int intType;
+        long int longInt;
+        long long int llint;
+        float floats;
     printf("Size of char: %zu byte(s)\n", sizeof(charType));
     printf("Size of int: %zu byte(s)\n", sizeof(intType));
     printf("Size of long int: %zu byte(s)\n", sizeof(longInt));

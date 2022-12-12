131ca13314192a79734d9ca136a79431405dc349
fem-itech <femielijaholubodun@gmail.com>
2022-12-11 19:29:40 -0800

    printit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry of the code
+ * Description: 'the program that print proper'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

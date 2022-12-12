650680146530ad61cdecf0c218843a97c71b9079
fem-itech <femielijaholubodun@gmail.com>
2022-12-11 19:21:48 -0800

    likepuzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry of the code
+ * Description: 'program to print programing is like puzzle'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

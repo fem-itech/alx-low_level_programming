7883a29e58acf695cd4536606e0e516280091878
fem-itech <femielijaholubodun@gmail.com>
2022-12-14 08:14:04 -0800

    2ndone

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Description: 'the program to print alphabet'
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c =a;
+
+    while (c <= 'z')
+    {
+	    _putchar(c)
+	    c++;
+    }
+    _putchar('\n');
+}

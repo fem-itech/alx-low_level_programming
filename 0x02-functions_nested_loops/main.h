2c2f813685922a2d2baddbd1644ba9d829b0f9b2
fem-itech <femielijaholubodun@gmail.com>
2022-12-14 08:00:31 -0800

    putchar1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,10 +8,10 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_baver(void);
+void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 int _putchar(char c);
 #endif

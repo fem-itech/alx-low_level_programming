4f6011927f14a25042ac32bd3131e32c3c1b65d3
fem-itech <femielijaholubodun@gmail.com>
2022-12-15 08:01:45 -0800

    printnumber

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int mul - function that multiply integer
+ * @c: mul to multiply 
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int a,b;
+	int x=a*b;
+
+

ee53c5ec0f20d1a43499d4b5982d1ed4bc89c17d
fem-itech <femielijaholubodun@gmail.com>
2022-12-15 08:20:45 -0800

    0-9else2and4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - print number from 0-9
+ * Execpt 2 and 4
+ * Return: no return
+ */
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+	if (ch != 2 && ch != 4)
+	{
+	_putchar(ch);
+	}
+	_putchar('\n');
+	}
+}

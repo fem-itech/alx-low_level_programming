ec05e4c0e00ffd32945beb95e162329cfa8f7c83
fem-itech <femielijaholubodun@gmail.com>
2022-12-15 08:23:24 -0800

    no4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,10 @@ void print_most_numbers(void)
 
 	for (ch = 0; ch <= 9; ch++)
 	{
-	if (ch != 2 && ch != 4)
-	{
-	_putchar(ch);
-	}
+		if (ch != 2 && ch != 4)
+		{
+		_putchar(ch);
+		}
 	_putchar('\n');
 	}
 }

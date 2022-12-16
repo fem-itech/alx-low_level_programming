dc1b0733a4fb4816c3e7abdf0c7aa547668c7cbe
fem-itech <femielijaholubodun@gmail.com>
2022-12-16 08:16:06 -0800

    letsave6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,19 +6,19 @@
  */
 void print_line(int n)
 {
-	int co;
+int co;
 
-	if (n <= 0)
-	{
-	_putchar ('\n')
-	}
+if (n <= 0)
+{
+_putchar ('\n');
+}
 
-	else
-	{
-	for (co = 1; co <= n; co++)
-	{
-	_putchar ('_');
-	}
-	_putchar ('\n');
-	}
+else
+{
+for (co = 1; co <= n; co++)
+{
+_putchar ('_');
+}
+_putchar ('\n');
+}
 }

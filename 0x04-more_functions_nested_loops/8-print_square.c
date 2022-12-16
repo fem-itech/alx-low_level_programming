4a05e4528590fbea6749bf59f3230836409dbda2
fem-itech <femielijaholubodun@gmail.com>
2022-12-16 09:18:45 -0800

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,23 +6,23 @@
  */
 void print_square(int size)
 {
-int co, ro
+	int co, ro
 
-if (size <= 0)
-{
-_putchar ('\n');
-}
-else
-{
-for (co = 1; co <= size; co++)
-{
-_putchar ('#');
+	if (size <= 0)
+	{
+	_putchar ('\n');
+	}
+	else
+	{
+		for (co = 1; co <= size; co++)
+		{
+		_putchar ('#');
 
-for (ro = 2; ro <= size; ro++)
-{
-_putchar ('#');
-}
-_putchar ('\n');
-}
-}
+			for (ro = 2; ro <= size; ro++)
+			{
+			_putchar ('#');
+			}
+		}
+	}
+	_putchar ('\n');
 }

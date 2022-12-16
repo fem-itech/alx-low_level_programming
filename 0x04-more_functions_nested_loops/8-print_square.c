db19c6ff97bc2e819b12dd5e292be7192cacb25c
fem-itech <femielijaholubodun@gmail.com>
2022-12-16 09:00:14 -0800

    now8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,18 +10,18 @@ void print_square(int size)
 
 	if (size <= 0)
 	{
-		_putchar ('\n');
+	_putchar ('\n');
 	}
 	else
 	{
-		for (co = 1; co <= size; co++)
-		{
-			_putchar ('#');
-		for (ro = 2; ro <= size; ro++)
-		{
-			_putchar ('#');
-		}
-			_putchar ('\n');
-		}
+	for (co = 1; co <= size; co++)
+	{
+	_putchar ('#');
+	for (ro = 2; ro <= size; ro++)
+	{
+	_putchar ('#');
+	}
+	_putchar ('\n');
+	}
 	}
 }

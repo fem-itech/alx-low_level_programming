21b0cf763187660152dcd7a936392cd244a8062d
fem-itech <femielijaholubodun@gmail.com>
2022-12-19 20:04:32 -0800

    feel9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	in count = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
+	while (count >= 0)
 	{
-		*(des + i) = *(src + i);
+		*(dest + count} = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
 	}
-
-	*(dest + i) = '\0';
 	return (dest);
-}

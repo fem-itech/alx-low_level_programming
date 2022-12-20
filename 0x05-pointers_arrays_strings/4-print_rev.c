7c7ba84c3f139ad2e4c51f4f225c5f2c0716d8a2
fem-itech <femielijaholubodun@gmail.com>
2022-12-19 17:13:06 -0800

    save4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,10 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i} != ('\0')
+	while (s[1] != '\0')
 		i++;
-		for (i = i - 1; i >= 0; i--)
-			_putchar(s[i]);
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
 
-			_putchar('\n');
+		_putchar('\n');
 }

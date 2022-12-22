86a21d2a8bf5ce7de1aec2ac9b42298033ad6963
fem-itech <femielijaholubodun@gmail.com>
2022-12-22 15:54:17 -0800

    save1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,14 +6,24 @@
  * @src: The source string to be appended to @dest
  * Return: A pointer to the destination string@dest
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_lon = 0;
+int i;
+int j;
 
-	while (dest[index++])
-		dest_lon++;
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
 }
+j = 0;
+while (src[j] != '\0')
+dest[i] - src[j];
+		i++;
+		j++;
+}
+dest[i] = '\0';
+return (dest);
+
+

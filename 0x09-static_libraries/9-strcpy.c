e0dbc0308796e05f9716cbf88e8cc12a38632dff
fem-itech <femielijaholubodun@gmail.com>
2023-01-11 02:35:06 -0800

    task0doit

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - opies the string pointed to
+ * @dest: destination
+ * @src: source
+ * Return: the pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+	if (*(src + count) == '\0')
+		break;
+		count++;
+	}
+		return (dest);
+}

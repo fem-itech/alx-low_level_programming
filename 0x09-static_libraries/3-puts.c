be0bcba7a9ba52c042e4ca278a7798d868e1d359
fem-itech <femielijaholubodun@gmail.com>
2023-01-11 00:16:51 -0800

    task00done

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,15 @@
 #include "main.h"
-#include<stdio.h>
+
 /**
- * main - Entry point
- * description: Programming is like building a multilingual puzzle
- * Return: Always 0 (Success)
+ * _puts - print a string
+ * @str: string to be printed
+ * Return: Always 0.
  */
-int main(void)
+void _puts(char *str)
 {
-	void _puts(char *str);
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
 }

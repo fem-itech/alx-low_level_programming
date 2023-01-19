db3db96d6fe369468ad53ace5a0328a725bcedab
fem-itech <femielijaholubodun@gmail.com>
2023-01-19 02:49:16 -0800

    savoo

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,11 +1,25 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-#include <stddef.h>
-#include <stdlib.h>
+#ifndef MAIN_H
 
-void print_name(char *name, void (*f)(char *));
+#define MAIN_H
+
+char *_memset(char *s, char b, unsigned int n);
+
 int _putchar(char c);
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_strchr(char *s, char c);
+
+unsigned int _strspn(char *s, char *accept);
+
+char *_strpbrk(char *s, char *accept);
+
+char *_strstr(char *haystack, char *needle);
+
+void print_chessboard(char (*a)[8]);
+
+void print_diagsums(int *a, int size);
+
+void set_string(char **s, char *to);
 
 #endif

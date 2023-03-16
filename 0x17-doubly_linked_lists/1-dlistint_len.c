e668c0ba0b9c6832ed860fea2014cf13548e252b
fem-itech <femielijaholubodun@gmail.com>
2023-03-16 13:38:21 -0700

    supdate1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,15 +7,23 @@
  * @h: head of the list
  * Return: the number of nodes
  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count;
 
-size_t dlistint_len(const dlistint_t *h) {
-    size_t count = 0; // Initialize the count variable to 0
+	count = 0;
 
-    // Traverse the linked list until you reach the end
-    while (h != NULL) {
-        count++; // Increment the count variable for every node you visit
-        h = h->next; // Move the pointer to the next node in the list
-    }
+	if (h == NULL)
+		return (count);
 
-    return count; // Return the total count of nodes in the list
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
